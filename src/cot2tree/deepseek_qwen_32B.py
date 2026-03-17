af07698344dde613dc2c54ec49466b1cd0ed2fd7
Bérénice <bjaulmes@yahoo.fr>
2026-03-17 14:00:13 +0100

    deepseek distilled to qwen 32B

---
+++ src/cot2tree/deepseek_qwen_32B.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# !pip install llama-cpp-python
+
+from llama_cpp import Llama
+
+llm = Llama.from_pretrained(
+	repo_id="bartowski/DeepSeek-R1-Distill-Qwen-32B-GGUF",
+	filename="DeepSeek-R1-Distill-Qwen-32B-IQ2_M.gguf",
+)
+llm.create_chat_completion(
+	messages = [
+		{
+			"role": "user",
+			"content": "What is the capital of France?"
+		}
+	]
+)
