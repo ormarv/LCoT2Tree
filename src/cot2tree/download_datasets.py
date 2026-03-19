14f25cb74d60d5744d1937ae285aed6e5e33f9ee
Bérénice <bjaulmes@yahoo.fr>
2026-03-19 14:06:54 +0100

    added filename to the hf download function

--- src/cot2tree/download_datasets.py
+++ src/cot2tree/download_datasets.py
@@ -1,3 +1,3 @@
 from huggingface_hub import hf_hub_download
 
-hf_hub_download(repo_id="microsoft/deberta-v3-large")
+hf_hub_download(repo_id="microsoft/deberta-v3-large", filename="deberta-v3-large")
