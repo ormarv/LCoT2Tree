fc5da0b1bcf22403de8751bbc8f9f6eda2fd3765
Bérénice <bjaulmes@yahoo.fr>
2026-03-20 16:52:34 +0100

    small bug

--- src/cot2tree/graph_construction.py
+++ src/cot2tree/graph_construction.py
@@ -64,7 +64,7 @@ def construct_graph(steps:Dict[int,str], threshold:float = 0.7)->Dict[str,List[s
         if not has_parent and step!=0:
             print(f"Sorted_scores: {sorted_scores}")
             graph.add_edge(list(sorted_scores.items())[0][0], step)
-            print(f"No satisfactory entailment. Adding {sorted_scores.items()[0][0]} as parent of {step}")
+            print(f"No satisfactory entailment. Adding {list(sorted_scores.items())[0][0]} as parent of {step}")
         # add to highest: what if no path gives satisfactory results?
         new_paths = get_all_new_paths(graph, step)
         paths[node] = new_paths
