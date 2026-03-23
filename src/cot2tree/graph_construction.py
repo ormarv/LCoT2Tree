5bfa6d949cb3353295053534d2bdd64ef2d11d09
jaulmes <bjaulmes@yahoo.fr>
2026-03-23 11:08:25 +0100

    print paths

--- src/cot2tree/graph_construction.py
+++ src/cot2tree/graph_construction.py
@@ -66,11 +66,12 @@ def construct_graph(steps:Dict[int,str], threshold:float = 0.7)->Dict[str,List[s
         if not has_parent and step!=0:
             print(f"Sorted_scores: {list(sorted_scores.items())}")
             graph.add_edge(list(sorted_scores.items())[0][0][0], step)
-            print(f"No satisfactory entailment. Adding {list(sorted_scores.items())[0][0]} as parent of {step}")
+            print(f"No satisfactory entailment. Adding {list(sorted_scores.items())[0][0][0]} as parent of {step}")
         # add to highest: what if no path gives satisfactory results?
         dict_graph = nx.to_dict_of_dicts(graph)
         print(f"The new graph is: {dict_graph}")
         new_paths = get_all_new_paths(graph, step)
         paths[step] = new_paths
+        print(f"New state of paths: {paths}")
     return graph
 
