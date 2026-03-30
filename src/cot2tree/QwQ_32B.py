35983e0bfe7fd9d003e1847955b1ab5d851ac627
jaulmes <bjaulmes@yahoo.fr>
2026-03-30 12:04:28 +0200

    downloading QwQ 32B

--- src/cot2tree/QwQ_32B.py
+++ src/cot2tree/QwQ_32B.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/QwQ-32B")
 model = AutoModelForCausalLM.from_pretrained("Qwen/QwQ-32B")
-messages = [
+"""messages = [
     {"role": "user", "content": "Who are you?"},
 ]
 inputs = tokenizer.apply_chat_template(
@@ -16,4 +16,4 @@ inputs = tokenizer.apply_chat_template(
 ).to(model.device)
 
 outputs = model.generate(**inputs, max_new_tokens=40)
-print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
+print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))"""
