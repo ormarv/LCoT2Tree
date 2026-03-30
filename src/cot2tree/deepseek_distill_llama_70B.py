2bf71d2626074e634d62fd73270e677226a00d67
jaulmes <bjaulmes@yahoo.fr>
2026-03-30 17:53:47 +0200

    Monday's overgit add src/cot2tree/deepseek_distill_llama_70B.py

--- src/cot2tree/deepseek_distill_llama_70B.py
+++ src/cot2tree/deepseek_distill_llama_70B.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Use a pipeline as a high-level helper
-from transformers import pipeline
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 pipe = pipeline("text-generation", model="unsloth/DeepSeek-R1-Distill-Llama-70B-GGUF")
 messages = [
