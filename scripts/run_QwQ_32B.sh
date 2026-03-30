c9b63bfb21f0b014e856dedfc3c73d8a02e7213a
jaulmes <bjaulmes@yahoo.fr>
2026-03-30 14:35:58 +0200

    downloading qwq on job

--- scripts/run_QwQ_32B.sh
+++ scripts/run_QwQ_32B.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=QwQ_test         # Name of your job
-#SBATCH --output=test_runs/%x_%j.out            # Output file (%x for job name, %j for job ID)
-#SBATCH --error=test_runs/%x_%j.err             # Error file
+#SBATCH --job-name=testclient         # Name of your job
+#SBATCH --output=load_qwq/%x_%j.out            # Output file (%x for job name, %j for job ID)
+#SBATCH --error=load_qwq/%x_%j.err             # Error file
+#SBATCH --partition=H100              # Partition to submit to (A100, V100, etc.)
 #SBATCH --nodes=1
 #SBATCH --gpus=1                     
-#SBATCH --time=00:05:00   
-#SBATCH --account=rqn@a100
-#SBATCH -C a100            
+#SBATCH --time=00:05:00               
 # Print job details
 echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
 
-# 1. Clear the environment (Good practice!)
-module purge
+# Define variables for your job
 
-# 3. Load Miniforge to restore conda/mamba to your PATH
-# (If 24.9.0 is unavailable, run `module avail miniforge` on the login node to find the latest)
-module load miniforge/24.9.0
+module load python/3.10.12
 
-# 4. Activate your environment
-# Note: On Jean Zay, it is generally safer to use `conda activate` even if you install with mamba
-mamba activate /lustre/fswork/projects/rech/rqn/ugy38tw/lcot2tree
-
+# Activate the environment
+source ~/TripleCoT/triplecot2/bin/activate
 
 # Execute the Python script with specific arguments
 #srun load_deltabench_gen_reasoning.py
-chmod +x src/cot2tree/QwQ_32B.py
 srun src/cot2tree/QwQ_32B.py
 #srun LLM-MindMap/edge_classification.py
 # Print job completion time
