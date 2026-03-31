36c6d53667f25fcc4f93fd60791de12f07910897
Bérénice <bjaulmes@yahoo.fr>
2026-03-31 17:31:44 +0200

    replacing which command with own script

--- scripts/run_QwQ_32B.sh
+++ scripts/run_QwQ_32B.sh
@@ -16,6 +16,7 @@ module load python/3.10.12
 echo "echo PATH"
 echo $PATH
 echo "Homemade which -a python"
+chmod +x ~/LCoT2Tree/scripts/mywhich
 ~/LCoT2Tree/scripts/mywhich -a python
 # Activate the environment
 source ~/LCoT2Tree/lcot2tree/bin/activate
