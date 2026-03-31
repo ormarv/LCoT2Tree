ce7f1b8ba05e99eba682fdb6953e568bf8ceae90
Bérénice <bjaulmes@yahoo.fr>
2026-03-31 16:17:57 +0200

    cleaning the prints

--- scripts/run_QwQ_32B.sh
+++ scripts/run_QwQ_32B.sh
@@ -11,8 +11,7 @@ echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
 
 # Define variables for your job
-echo "echo PATH"
-echo $PATH
+
 module load python/3.10.12
 echo "echo PATH"
 echo $PATH
@@ -28,20 +27,10 @@ echo "ls -l /home/infres/bjaulmes-22/LCoT2Tree/lcot2tree/bin/py*"
 ls -l /home/infres/bjaulmes-22/LCoT2Tree/lcot2tree/bin/py*
 echo "which python"
 which python
-echo "ls /lib64/libbz2*"
-ls /lib64/libbz2*
-echo "ln -s /lib64/libbz2.so.1 ~/.cache/libz2_alma_linux/libbz2.so.1.0"
-ln -s /lib64/libbz2.so.1 ~/.cache/libz2_alma_linux/libbz2.so.1.0
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.cache/libbz2_alma_linux/
-if test -f /lib64/libbz2.so.1; then
-    echo "/lib64/libbz2.so.1 found"
-fi
-if test -f ~/.cache/libz2_alma_linux/libbz2.so.1.0; then
-    echo "~/.cache/libz2_alma_linux/libbz2.so.1.0 found"
-fi
 # Execute the Python script with specific arguments
 #srun load_deltabench_gen_reasoning.py
 which python
+echo "which pip"
 which pip
 pip list
 srun src/cot2tree/QwQ_32B.py
