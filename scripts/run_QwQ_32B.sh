2a8f70ccb966100c60e1805127a2be7795fdd37f
Bérénice <bjaulmes@yahoo.fr>
2026-03-31 14:47:36 +0200

    prints, prints everywhere

--- scripts/run_QwQ_32B.sh
+++ scripts/run_QwQ_32B.sh
@@ -11,20 +11,24 @@ echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
 
 # Define variables for your job
+echo "echo PATH"
 echo $PATH
 module load python/3.10.12
-echo "PATH"
+echo "echo PATH"
 echo $PATH
-echo "Python"
+echo "which python"
 which python
 # Activate the environment
 source ~/LCoT2Tree/lcot2tree/bin/activate
-echo "PATH"
+echo "echo PATH"
 echo $PATH
 echo "ls -l /home/infres/bjaulmes-22/LCoT2Tree/lcot2tree/bin/py*"
 ls -l /home/infres/bjaulmes-22/LCoT2Tree/lcot2tree/bin/py*
+echo "which python"
 which python
+echo "ls /lib64/libbz2*"
 ls /lib64/libbz2*
+echo "ln -s /lib64/libbz2.so.1 ~/.cache/libz2_alma_linux/libbz2.so.1.0"
 ln -s /lib64/libbz2.so.1 ~/.cache/libz2_alma_linux/libbz2.so.1.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.cache/libbz2_alma_linux/
 if test -f /lib64/libbz2.so.1; then
