f5ee900f24bc5eaf3b116a98b632c981d55481b9
Bérénice <bjaulmes@yahoo.fr>
2026-04-02 11:30:56 +0200

    second run

--- scripts/construct_graph.sh
+++ scripts/construct_graph.sh
@@ -28,8 +28,8 @@ mamba activate /lustre/fswork/projects/rech/rqn/ugy38tw/lcot2tree
 #srun load_deltabench_gen_reasoning.py
 chmod +x src/cot2tree/split_lcot.py
 #srun src/cot2tree/split_lcot.py -m 5
-srun src/cot2tree/split_lcot.py
-#srun src/cot2tree/split_lcot.py -t2 0.5
+#srun src/cot2tree/split_lcot.py
+srun src/cot2tree/split_lcot.py -t2 0.5
 #srun src/cot2tree/split_lcot.py -m 5 -t2 0.5
 #srun LLM-MindMap/edge_classification.py
 # Print job completion time
