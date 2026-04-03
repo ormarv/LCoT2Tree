63d48f8100ed545f7f81b0337009c891a033226b
Bérénice <bjaulmes@yahoo.fr>
2026-04-03 16:24:00 +0200

    extending time limit and switching to V100

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -4,9 +4,11 @@
 #SBATCH --error=statistics/%x_%j_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
+#SBATCH -C a100
+#SBATCH --gres=gpu:1
 #SBATCH --hint=nomultithread
-#SBATCH --time=01:00:00
-#SBATCH --account=rqn@cpu
+#SBATCH --time=05:00:00
+#SBATCH --account=rqn@a100
 
 echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
