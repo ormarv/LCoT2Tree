8ff25960c10cbbe86931acec23b25b7bce0acc84
Bérénice <bjaulmes@yahoo.fr>
2026-04-03 14:56:56 +0200

    make the non-existing directories

--- src/cot2tree/make_graph_statistics.py
+++ src/cot2tree/make_graph_statistics.py
@@ -18,6 +18,10 @@ samples = gen_reasoning.select(indices)
 print(samples)
 #run split_lcot on each and save results in log files
 lcots = [sample['long_cot'] for sample in samples]
+directories = ["./no_max_no_t2", "./no_max_t2_0_5", "./max_5_no_t2", "./max_5_t2_0_5", "./max_1_no_t2", "./max_1_t2_0_5"]
+for directory in directories:
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
 graphs_no_max_no_t2 = [build_graph_from_chain(lcot,max_path_length_for_nli=None, logfile=open(f"./no_max_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 graphs_no_max_t2_0_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=None, t2=0.5, logfile=open(f"./no_max_t2_0_5/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 graphs_max_5_no_t2 = [build_graph_from_chain(lcot, logfile=open(f"./max_5_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
@@ -28,7 +32,6 @@ graphs_max_1_t2_0_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=1, t
 #make statistics
 # For each directory, get the list of files
 # For each file in the list, count the number of "Adding ", "No satisfactory entailment", and "semi-default"
-directories = ["./no_max_no_t2", "./no_max_t2_0_5", "./max_5_no_t2", "./max_5_t2_0_5", "./max_1_no_t2", "./max_1_t2_0_5"]
 for dir in directories:
     print(f"Directory: {dir}")
     list_edges = []
