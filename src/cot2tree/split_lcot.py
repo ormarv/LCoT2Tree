b64263a87729beb37a20f9c0549f7ef83f3aca7d
jaulmes <bjaulmes@yahoo.fr>
2026-04-07 15:22:02 +0200

    printing the string we use for regex

--- src/cot2tree/split_lcot.py
+++ src/cot2tree/split_lcot.py
@@ -60,6 +60,7 @@ def intelligent_split(lcot:str, n_first:int, logfile:TextIOWrapper):
         augmented_keywords.append(capitalized+",")
     print(augmented_keywords, file=logfile)
     string = '|'.join(augmented_keywords)
+    print(f"Regex string: {string}")
     steps = re.finditer(string,lcot)
     split_indices = []
     for match in steps:
