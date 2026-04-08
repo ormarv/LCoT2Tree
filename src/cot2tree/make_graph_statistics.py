2fc5d07ff4ceeed972aee6b79e8bc50b3363abcc
Bérénice <bjaulmes@yahoo.fr>
2026-04-08 16:54:06 +0200

    just the statistics, commented the rest for now

--- src/cot2tree/make_graph_statistics.py
+++ src/cot2tree/make_graph_statistics.py
@@ -6,23 +6,23 @@ import os
 from split_lcot import build_graph_from_chain
 
 #take 30 deltabench samples
-dataset = load_dataset("csv", split="train", data_files="../.cache/huggingface/hub/datasets--OpenStellarTeam--DeltaBench/snapshots/894d233e5beb06b312b29761cc10e10ca5d2588a/Deltabench_v1.csv")
+#dataset = load_dataset("csv", split="train", data_files="../.cache/huggingface/hub/datasets--OpenStellarTeam--DeltaBench/snapshots/894d233e5beb06b312b29761cc10e10ca5d2588a/Deltabench_v1.csv")
 #print(dataset)
-gen_reasoning = dataset.filter(lambda sample: sample['task_l1']=='general')
+#gen_reasoning = dataset.filter(lambda sample: sample['task_l1']=='general')
 #print(len(gen_reasoning))
-random.seed(42)
-indices = [int(random.random()*50) for _ in range(50)]
+#random.seed(42)
+#indices = [int(random.random()*50) for _ in range(50)]
 #print(indices)
 #print(type(gen_reasoning))
 #print(gen_reasoning[0])
-samples = gen_reasoning.select(indices)
+#samples = gen_reasoning.select(indices)
 #print(samples)
 #run split_lcot on each and save results in log files
-lcots = [sample['long_cot'] for sample in samples]
+#lcots = [sample['long_cot'] for sample in samples]
 directories = ["$WORK/.local/no_max_no_t2", "$WORK/.local/no_max_t2_0_5", "$WORK/.local/max_5_no_t2", "$WORK/.local/max_5_t2_0_5", "$WORK/.local/max_1_no_t2", "$WORK/.local/max_1_t2_0_5"]
-for directory in directories:
+"""for directory in directories:
     if not os.path.isdir(directory):
-        os.makedirs(directory)
+        os.makedirs(directory)"""
 """print("The first one.")
 print(lcots[0])
 print("And the second one")
@@ -31,7 +31,7 @@ print(f"Are samples 0 and 1 identical?: {lcots[0]==lcots[1]}")
 print(f"Are samples 0 and 2 identical?: {lcots[0]==lcots[2]}")
 print(f"Are samples 0 and 3 identical?: {lcots[0]==lcots[3]}")
 print(f"Are samples 0 and 4 identical?: {lcots[0]==lcots[4]}")"""
-graphs_no_max_no_t2 = [build_graph_from_chain(lcot,max_path_length_for_nli=None, logfile=open(f"$WORK/.local/no_max_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+"""graphs_no_max_no_t2 = [build_graph_from_chain(lcot,max_path_length_for_nli=None, logfile=open(f"$WORK/.local/no_max_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 print("Done with 0: No max, no t2.")
 graphs_no_max_t2_0_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=None, t2=0.5, logfile=open(f"$WORK/.local/no_max_t2_0_5/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 print("Done with 1: No max, t2 = 0.5.")
@@ -42,7 +42,7 @@ print("Done with 3: max = 5, t2 = 0.5.")
 graphs_max_1_no_t2 = [build_graph_from_chain(lcot, max_path_length_for_nli=1, logfile=open(f"$WORK/.local/max_1_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 print("Done with 4: max = 1, no t2")
 graphs_max_1_t2_0_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=1, t2=0.5, logfile=open(f"$WORK/.local/max_1_t2_0_5/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
-print("Done with 5: max = 1, t2 = 0.5.")
+print("Done with 5: max = 1, t2 = 0.5.")"""
 #make statistics
 # For each directory, get the list of files
 # For each file in the list, count the number of "Adding ", "No satisfactory entailment", and "semi-default"
