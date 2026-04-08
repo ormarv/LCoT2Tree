2e997f01729c053d21b89f0e153aedffe2dc23d3
jaulmes <bjaulmes@yahoo.fr>
2026-04-08 19:34:17 +0200

    asking for less computing time

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -7,7 +7,7 @@
 #SBATCH -C v100
 #SBATCH --gres=gpu:1
 #SBATCH --hint=nomultithread
-#SBATCH --time=05:00:00
+#SBATCH --time=04:00:00
 #SBATCH --account=rqn@v100
 
 echo "Starting job on node: $(hostname)"
