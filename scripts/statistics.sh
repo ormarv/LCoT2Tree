50667e639e32a7f5e979195ee5710b125ae60686
Orma <bjaulmes@yahoo.fr>
2026-04-20 18:38:17 +0200

    trying to load a cuda module (one that exists on JZ)

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH -C v100
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --hint=nomultithread
 #SBATCH --time=00:30:00
 #SBATCH --account=rqn@v100
@@ -20,7 +20,7 @@ module purge
 # 3. Load Miniforge to restore conda/mamba to your PATH
 # (If 24.9.0 is unavailable, run `module avail miniforge` on the login node to find the latest)
 module load miniforge/24.9.0
-module load cuda/12.1
+module load cuda/12.4.1
 # 4. Activate your environment
 # Note: On Jean Zay, it is generally safer to use `conda activate` even if you install with mamba
 conda activate /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot
