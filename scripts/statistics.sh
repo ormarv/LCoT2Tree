fffbe01446b370eee5f93f9db8e7604f225b65a8
Orma <bjaulmes@yahoo.fr>
2026-04-21 13:31:58 +0200

    graph statistics for step fusion

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH -C v100
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --hint=nomultithread
 #SBATCH --time=00:30:00
 #SBATCH --account=rqn@v100
@@ -20,16 +20,16 @@ module purge
 # 3. Load Miniforge to restore conda/mamba to your PATH
 # (If 24.9.0 is unavailable, run `module avail miniforge` on the login node to find the latest)
 module load miniforge/24.9.0
-module load cuda/12.4.1
+#module load cuda/12.4.1
 # 4. Activate your environment
 # Note: On Jean Zay, it is generally safer to use `conda activate` even if you install with mamba
 conda activate /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot
 
 # 5. Run your script with the '-u' (unbuffered) flag
-#chmod +x src/cot2tree/make_graph_statistics.py
-#srun src/cot2tree/make_graph_statistics.py
-echo $LLAMA_CPP_LIB
-export LLAMA_CPP_LIB=/linkhome/rech/genltc01/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/libllama.so
-chmod +x src/cot2tree/language_models.py
-ls /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/
-srun src/cot2tree/language_models.py
+chmod +x src/cot2tree/make_graph_statistics.py
+srun src/cot2tree/make_graph_statistics.py
+#echo $LLAMA_CPP_LIB
+#export LLAMA_CPP_LIB=/linkhome/rech/genltc01/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/libllama.so
+#chmod +x src/cot2tree/language_models.py
+#ls /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/
+#srun src/cot2tree/language_models.py
--- src/cot2tree/make_graph_statistics.py
+++ src/cot2tree/make_graph_statistics.py
@@ -2,27 +2,28 @@
 from datasets import Dataset, load_dataset
 import random
 import os
+from tqdm import tqdm
 
 from split_lcot import build_graph_from_chain
 
 #take 30 deltabench samples
-#dataset = load_dataset("csv", split="train", data_files="../.cache/huggingface/hub/datasets--OpenStellarTeam--DeltaBench/snapshots/894d233e5beb06b312b29761cc10e10ca5d2588a/Deltabench_v1.csv")
+dataset = load_dataset("csv", split="train", data_files="../.cache/huggingface/hub/datasets--OpenStellarTeam--DeltaBench/snapshots/894d233e5beb06b312b29761cc10e10ca5d2588a/Deltabench_v1.csv")
 #print(dataset)
-#gen_reasoning = dataset.filter(lambda sample: sample['task_l1']=='general')
+gen_reasoning = dataset.filter(lambda sample: sample['task_l1']=='general')
 #print(len(gen_reasoning))
-#random.seed(42)
-#indices = [int(random.random()*50) for _ in range(50)]
+random.seed(42)
+indices = [int(random.random()*50) for _ in range(50)]
 #print(indices)
 #print(type(gen_reasoning))
 #print(gen_reasoning[0])
-#samples = gen_reasoning.select(indices)
+samples = gen_reasoning.select(indices)
 #print(samples)
 #run split_lcot on each and save results in log files
-#lcots = [sample['long_cot'] for sample in samples]
-directories = ["/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_t2_0_5", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_1_no_t2", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_1_t2_0_5"]
-"""for directory in directories:
+lcots = [sample['long_cot'] for sample in samples]
+directories = ["/lustre/fswork/projects/rech/rqn/ugy38tw/.local/no_max_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_3_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_4_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_6_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_7_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_8_fusion", "/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_10_fusion"]
+for directory in directories:
     if not os.path.isdir(directory):
-        os.makedirs(directory)"""
+        os.makedirs(directory)
 """print("The first one.")
 print(lcots[0])
 print("And the second one")
@@ -31,12 +32,24 @@ print(f"Are samples 0 and 1 identical?: {lcots[0]==lcots[1]}")
 print(f"Are samples 0 and 2 identical?: {lcots[0]==lcots[2]}")
 print(f"Are samples 0 and 3 identical?: {lcots[0]==lcots[3]}")
 print(f"Are samples 0 and 4 identical?: {lcots[0]==lcots[4]}")"""
-"""graphs_no_max_no_t2 = [build_graph_from_chain(lcot,max_path_length_for_nli=None, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/no_max_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
-print("Done with 0: No max, no t2.")
-graphs_no_max_t2_0_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=None, t2=0.5, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/no_max_t2_0_5/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
-print("Done with 1: No max, t2 = 0.5.")
-graphs_max_5_no_t2 = [build_graph_from_chain(lcot, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
-print("Done with 2: max = 5, no t2.")"""
+graphs_no_max = [build_graph_from_chain(lcot,max_path_length_for_nli=None, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/no_max_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 0: No max")
+graphs_max_3 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_3_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 1: Max 3")
+graphs_max_4 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_4_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 2: Max 4")
+graphs_max_5 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 3: Max 5")
+graphs_max_6 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_6_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 4: Max 6")
+graphs_max_7 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_7_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 5: Max 7")
+graphs_max_8 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_8_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 6: Max 8")
+graphs_max_10 = [build_graph_from_chain(lcot, max_path_length_for_nli=3, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_10_fusion/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+print("Done with 7: Max 10")
+#graphs_max_5_no_t2 = [build_graph_from_chain(lcot, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
+#print("Done with 2: max = 5, no t2.")
 """graphs_max_5_t2_0_5 = [build_graph_from_chain(lcot, t2=0.5, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_5_t2_0_5/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
 print("Done with 3: max = 5, t2 = 0.5.")
 graphs_max_1_no_t2 = [build_graph_from_chain(lcot, max_path_length_for_nli=1, logfile=open(f"/lustre/fswork/projects/rech/rqn/ugy38tw/.local/max_1_no_t2/{i}.txt","w+")) for i, lcot in enumerate(lcots)]
@@ -46,7 +59,7 @@ print("Done with 5: max = 1, t2 = 0.5.")"""
 #make statistics
 # For each directory, get the list of files
 # For each file in the list, count the number of "Adding ", "No satisfactory entailment", and "semi-default"
-for dir in directories:
+for dir in tqdm(directories):
     print(f"Directory: {dir}")
     list_edges = []
     list_semi_default_parents = []
