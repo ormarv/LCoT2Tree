8d7239b9504d3945dda764cc350e6739ffc4d367
Orma <bjaulmes@yahoo.fr>
2026-04-21 17:32:31 +0200

    let's try to run QwQ32B on JZ

--- scripts/cpp.sh
+++ scripts/cpp.sh
@@ -20,14 +20,11 @@ module purge
 # 3. Load Miniforge to restore conda/mamba to your PATH
 # (If 24.9.0 is unavailable, run `module avail miniforge` on the login node to find the latest)
 module load miniforge/24.9.0
-module load cuda/12.4.1
+
 # 4. Activate your environment
 # Note: On Jean Zay, it is generally safer to use `conda activate` even if you install with mamba
 conda activate /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot
 
 # 5. Run the script
-echo $LLAMA_CPP_LIB
-export LLAMA_CPP_LIB=/linkhome/rech/genltc01/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/libllama.so
-chmod +x src/cot2tree/language_models.py
-ls /lustre/fswork/projects/rech/rqn/ugy38tw/triplecot/lib/python3.10/site-packages/llama_cpp/lib/
-srun src/cot2tree/language_models.py
+chmod +x src/cot2tree/QwQ_32B.py
+srun src/cot2tree/QwQ_32B.py
