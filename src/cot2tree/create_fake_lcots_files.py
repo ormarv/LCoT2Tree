6b26cfcd97617d7d86ba65a192715209a5914748
Orma <bjaulmes@yahoo.fr>
2026-04-28 15:37:08 +0200

    adding the file i need to run

---
+++ src/cot2tree/create_fake_lcots_files.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+import numpy as np
+
+dataset = load_dataset("~/.cache/huggingface/hub/datasets--OpenStellarTeam--DeltaBench/snapshots/894d233e5beb06b312b29761cc10e10ca5d2588a/Deltabench_v1.csv")["train"]
+train_labels = np.random.randint(0, 2, 10)
+eval_labels = np.random.randint(0, 2, 10)
+features = ['nb_parents', 'nb_children', 'node_index', 'distance_to_end', 'nb_words_before', 'nb_nodes_per_depth']
+test_labels = {feature:np.random.randint(0,2,2) for feature in features}
+with open("~/.local/lcots/train.txt", "w+") as f:
+    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(train_labels[i]) for i in range(10)]))
+with open("~/.local/lcots/eval.txt", "w+") as f:
+    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(eval_labels[i]) for i in range(10, 20)]))
+for feature in features:
+    with open(f"~/.local/lcots/test_{feature}.txt", "w+") as f:
+        print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(train_labels[i]) for i in range(10)]))
+  
+    
+
