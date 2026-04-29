e04b1a6e2ec63bb689c7b20cbd4481e28bd3ceef
Orma <bjaulmes@yahoo.fr>
2026-04-29 14:29:22 +0200

    another debug print

--- src/cot2tree/main.py
+++ src/cot2tree/main.py
@@ -65,7 +65,7 @@ if "train" in actions:
                 if "train" in file:
                     if verbose:
                         print(f"Loading train graphs from file {path}.")
-                    c = f.read().split("############")
+                    c = f.read().split("############")[0].split("&&&&&&&&&&&&")
                     print(f"Content: {c}")
                     train_graphs_with_full_features = [(json.loads(content.split("&&&&&&&&&&&&")[0]), [feature.split(',') for feature in content.split("&&&&&&&&&&&&")[1].split(',')], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
                     # For each graph, we need a json.loads, for the features a split on "," twice, and for the labels a transformation to boolean form.
