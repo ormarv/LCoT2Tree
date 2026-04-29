1cdc29179c726b53986b61ddd35e578bb79a00f3
Orma <bjaulmes@yahoo.fr>
2026-04-29 13:56:29 +0200

    empty train.txt fixed

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -31,7 +31,7 @@ chmod +x src/cot2tree/main.py
 chmod +x src/cot2tree/create_fake_lcots_files.py
 chmod -R +x ../.local/graphs
 chmod -R +x ../.local/lcots
-srun src/cot2tree/gatv2.py
-#srun src/cot2tree/main.py train -g -d ~/.local/graphs -v -F nb_parents nb_children node_index distance_to_end nb_words_before nb_nodes_per_depth
+#srun src/cot2tree/gatv2.py
+srun src/cot2tree/main.py train -g -d ~/.local/graphs -v -F nb_parents nb_children node_index distance_to_end nb_words_before nb_nodes_per_depth
 
 echo "Job ended at: $(date)"
