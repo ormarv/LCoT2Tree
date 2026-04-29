e14e292dce51477c345d45ecdf57705289abbd64
Orma <bjaulmes@yahoo.fr>
2026-04-29 17:00:26 +0200

    extracting features with ast.literal_eval instead of splitting on commas

--- src/cot2tree/main.py
+++ src/cot2tree/main.py
@@ -72,12 +72,12 @@ if "train" in actions:
                     print(f"Trying ast.literal_eval: {ast.literal_eval(a)}")
                     print(f"Features: {b}")
                     print(f"Label: {c}")"""
-                    train_graphs_with_full_features = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [feature.split(',') for feature in content.split("&&&&&&&&&&&&")[1].split(',')], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
+                    train_graphs_with_full_features = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [ast.literal_eval(content.split("&&&&&&&&&&&&")[1])], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
                     # For each graph, we need a ast.literal_eval, for the features a split on "," twice, and for the labels a transformation to boolean form.
                 if "eval" in file:
                     if verbose:
                         print(f"Loading eval graphs from file {path}.")
-                    eval_graphs_with_full_features = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [feature.split(',') for feature in content.split("&&&&&&&&&&&&")[1].split(',')], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
+                    eval_graphs_with_full_features = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [ast.literal_eval(content.split("&&&&&&&&&&&&")[1])], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
     
     else:
         if args.use_existing_lcots:  # If we use pre-existing LCoTs
@@ -175,7 +175,7 @@ if "test" in actions:
                 with open(path, "w+") as f:
                     if verbose:
                         print(f"Loading test graphs on subject {subject} from file {path}.")
-                    test_graphs_with_full_features[subject] = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [feature.split(',') for feature in content.split("&&&&&&&&&&&&")[1].split(',')], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
+                    test_graphs_with_full_features[subject] = [(nx.from_dict_of_dicts(ast.literal_eval(content.split("&&&&&&&&&&&&")[0])), [ast.literal_eval(content.split("&&&&&&&&&&&&")[1])], eval(content.split("&&&&&&&&&&&&")[2])) for content in f.read().split("############")]
     else:
         if args.use_existing_lcots:  # If we use pre-existing LCoTs
             if verbose:
