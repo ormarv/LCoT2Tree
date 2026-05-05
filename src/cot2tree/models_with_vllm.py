a17ce75fd2bfc0751990794cfbd32289b3854f95
Orma <bjaulmes@yahoo.fr>
2026-05-05 13:33:17 +0200

    trying bitsandbytes in 4bit

--- src/cot2tree/models_with_vllm.py
+++ src/cot2tree/models_with_vllm.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_id = "models--unsloth--DeepSeek-R1-Distill-Llama-70B-GGUF/snapshots/732dd974083ea5877d7b6d788b36fe7c2e5eab36/"
-quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+quantization_config = BitsAndBytesConfig(load_in_4bit=True)
 
 quantized_model = AutoModelForCausalLM.from_pretrained(
     model_id, device_map="auto", torch_dtype=torch.bfloat16, quantization_config=quantization_config)
