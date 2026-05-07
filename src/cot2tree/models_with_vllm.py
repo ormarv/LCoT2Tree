c456fab5ed327f5a4df21fbf4c29e5f1ea5b9081
Orma <bjaulmes@yahoo.fr>
2026-05-07 09:48:07 +0200

    now trying DeepSeek-R1-Distill-Qwen32B

--- src/cot2tree/models_with_vllm.py
+++ src/cot2tree/models_with_vllm.py
@@ -2,7 +2,8 @@
 
 from vllm import LLM, SamplingParams
 import torch
-model_id = "/linkhome/rech/genltc01/ugy38tw/.cache/huggingface/hub/models--deepseek-ai--DeepSeek-R1-Distill-Llama-70B/snapshots/b1c0b44b4369b597ad119a196caf79a9c40e141e"
+#model_id = "/linkhome/rech/genltc01/ugy38tw/.cache/huggingface/hub/models--deepseek-ai--DeepSeek-R1-Distill-Llama-70B/snapshots/b1c0b44b4369b597ad119a196caf79a9c40e141e"
+model_id = "/linkhome/rech/genltc01/ugy38tw/.cache/huggingface/hub/models--deepseek-ai--DeepSeek-R1-Distill-Qwen-32B/snapshots/711ad2ea6aa40cfca18895e8aca02ab92df1a746/"
 llm = LLM(
     
 model=model_id,
