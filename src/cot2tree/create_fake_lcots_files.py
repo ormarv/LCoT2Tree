fbf5fc3c98399b9ef8c6aeb2ca43b2bebb3d7957
jaulmes <bjaulmes@yahoo.fr>
2026-05-08 10:29:11 +0200

    testing with less data

--- src/cot2tree/create_fake_lcots_files.py
+++ src/cot2tree/create_fake_lcots_files.py
@@ -8,9 +8,9 @@ eval_labels = np.random.randint(0, 2, 10)
 subjects = {'sub1':0, 'sub2':1}
 test_labels = {subject:np.random.randint(0,2,2) for subject in subjects}
 with open("../.local/lcots/train.txt", "w+") as f:
-    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(train_labels[i]) for i in range(10)]), file=f)
+    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(train_labels[i]) for i in range(3)]), file=f)
 with open("../.local/lcots/eval.txt", "w+") as f:
-    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(eval_labels[i]) for i in range(10)]), file=f)
+    print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(eval_labels[i]) for i in range(3)]), file=f)
 for subject in subjects:
     with open(f"../.local/lcots/test_{subject}.txt", "w+") as f:
         print("############".join([dataset[i]["long_cot"]+"&&&&&&&&&&&&"+str(test_labels[subject][i]) for i in range(2)]), file=f)
