b12462a7f26020a5577dd4230693d87726bb9db8
jaulmes <bjaulmes@yahoo.fr>
2026-05-11 16:32:35 +0200

    more GPUsgit add *

--- scripts/cpp.sh
+++ scripts/cpp.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH -C h100
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --hint=nomultithread
 #SBATCH --time=01:00:00
 #SBATCH --account=rqn@h100
