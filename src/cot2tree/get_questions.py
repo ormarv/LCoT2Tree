4284178a15196270d882ddd22d9c6f190f076a26
jaulmes <bjaulmes@yahoo.fr>
2026-05-11 16:20:11 +0200

    enable_chunked_prefill

--- src/cot2tree/get_questions.py
+++ src/cot2tree/get_questions.py
@@ -114,6 +114,7 @@ def run_model_with_vLLM(model_id:str, queries:List[str]):
     tensor_parallel_size=2,
     gpu_memory_utilization=0.9,
     kv_cache_dtype="fp8",
+    enable_chunked_prefill=True
     )
 
     max_model_len = llm.llm_engine.model_config.max_model_len
