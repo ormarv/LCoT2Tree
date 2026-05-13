80dd63dd77641c794801b412b1a1de3386aaeca7
Orma <bjaulmes@yahoo.fr>
2026-05-13 16:14:23 +0200

    adding tqdm for QwQ's queries

--- src/cot2tree/QwQ_32B.py
+++ src/cot2tree/QwQ_32B.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from typing import List
+from tqdm import tqdm
 #model_name = "/lustre/fswork/projects/rech/rqn/ugy38tw/.cache/huggingface/hub/models--bartowski--DeepSeek-R1-Distill-Qwen-32B-GGUF/snapshots/1dc8cf9ffa5dd333057ea1b09ccf4772d8726dec/DeepSeek-R1-Distill-Qwen-32B-Q8_0.gguf"
 model_name = "/lustre/fswork/projects/rech/rqn/ugy38tw/.cache/huggingface/hub/models--unsloth--DeepSeek-R1-Distill-Llama-70B-GGUF/snapshots/732dd974083ea5877d7b6d788b36fe7c2e5eab36/"
 #model_name = "/lustre/fswork/projects/rech/rqn/ugy38tw/.cache/huggingface/hub/models--Qwen--QwQ-32B/snapshots/976055f8c83f394f35dbd3ab09a285a984907bd0/"
@@ -9,7 +10,7 @@ def run_QwQ32B(queries:str)->List[str]:
 	tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path = "/lustre/fswork/projects/rech/rqn/ugy38tw/.cache/huggingface/hub/models--Qwen--QwQ-32B/snapshots/976055f8c83f394f35dbd3ab09a285a984907bd0/")
 	model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path = "/lustre/fswork/projects/rech/rqn/ugy38tw/.cache/huggingface/hub/models--Qwen--QwQ-32B/snapshots/976055f8c83f394f35dbd3ab09a285a984907bd0/")
 	answers = []
-	for query in queries:
+	for query in tqdm(queries):
 		messages = [
 			{"role": "user", "content": query},
 		]
