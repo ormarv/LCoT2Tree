3248fd35de66b4e0d669f6c95d563c6adc7b9e94
jaulmes <bjaulmes@yahoo.fr>
2026-05-14 17:25:08 +0200

    let's hope this is too buggy

--- scripts/cpp.sh
+++ scripts/cpp.sh
@@ -9,7 +9,7 @@
 #SBATCH -C h100
 #SBATCH --gres=gpu:4
 #SBATCH --hint=nomultithread
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --account=rqn@h100
 #SBATCH --qos=qos_gpu_h100-dev
 echo "Starting job on node: $(hostname)"
