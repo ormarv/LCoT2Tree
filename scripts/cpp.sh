7f98d66ea180c2a1d4491d946e651266d2f566c6
jaulmes <bjaulmes@yahoo.fr>
2026-05-19 22:14:47 +0200

    just one last test!

--- scripts/cpp.sh
+++ scripts/cpp.sh
@@ -2,15 +2,16 @@
 #SBATCH --job-name=cpp
 #SBATCH --output=/lustre/fswork/projects/rech/rqn/ugy38tw/.local/cpp/%x_%j_%a.out
 #SBATCH --error=/lustre/fswork/projects/rech/rqn/ugy38tw/.local/cpp/%x_%j_%a.err
-#SBATCH --array=1-12
+#SBATCH --array=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 #SBATCH -C h100
 #SBATCH --gres=gpu:4
 #SBATCH --hint=nomultithread
-#SBATCH --time=15:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=rqn@h100
+#SBATCH --qos=qos_gpu_h100‑dev
 
 echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
