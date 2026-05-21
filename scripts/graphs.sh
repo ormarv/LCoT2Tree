f518fa3ee3036d83113b6c5654dea30dbef213d5
jaulmes <bjaulmes@yahoo.fr>
2026-05-22 00:43:33 +0200

    relaunching graphs

--- scripts/graphs.sh
+++ scripts/graphs.sh
@@ -9,7 +9,7 @@
 #SBATCH -C v100
 #SBATCH --gres=gpu:1
 #SBATCH --hint=nomultithread
-#SBATCH --time=8:00:00
+#SBATCH --time=14:00:00
 #SBATCH --account=rqn@v100
 
 echo "Starting job on node: $(hostname)"
